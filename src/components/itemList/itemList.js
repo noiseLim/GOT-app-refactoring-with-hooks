b6816a9bee3e91351d7d3a17f1b31964f54417ff
noiseLim <noiselim@gmail.com>
2020-12-16 21:40:38 +0300

    add catch error in itemList

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,34 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import './itemList.css';
 import Spinner from '../spinner';
-// import ErrorMessage from '../errorMessage';
+import ErrorMessage from '../errorMessage';
 
 function ItemList({getData, renderItem, onItemSelected}) {
 
     const [itemList, updateList] = useState([]);
-    // const [error, onError] = useState(false);
+    const [error, onError] = useState(false);
 
     useEffect(() => {
         getData() 
             .then((data) => {
                 updateList(data)
             })
-            // .catch(() => onError());
+            .catch(() => onError(true));
     }, [])
-
-    // componentDidCatch() {
-    //     this.setState({
-    //         itemList: null,
-    //         error: true
-    //     })
-    // }
-
-    // onError() {
-    //     this.setState({
-    //         itemList: null,
-    //         error: true
-    //     })
-    // }
 
     function renderItems(arr) {
         return arr.map((item) => {
@@ -45,13 +31,13 @@ function ItemList({getData, renderItem, onItemSelected}) {
         })
     }
 
-    // if (this.state.error) {
-    //     return (
-    //         <div className="random-block rounded">
-    //             <ErrorMessage/>
-    //         </div>
-    //     )
-    // }
+    if (error) {
+        return (
+            <div className="random-block rounded">
+                <ErrorMessage/>
+            </div>
+        )
+    }
 
     if (!itemList) {
         return (
